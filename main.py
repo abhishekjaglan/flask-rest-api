7a6b77c572ae01f3ffaff02706ec56d06d014326
Abhishek Jaglan <jaglanabhishek08@gmail.com>
2024-11-26 10:16:18 +0530

    updated

--- main.py
+++ main.py
@@ -21,9 +21,9 @@ class VideoModel(db.Model):
 
 ## validation of input required, validates all arguemnts' type as well as makes sure mandatory args are retrieved othertwise throws error
 video_put_ags = reqparse.RequestParser()
-video_put_ags.add_argument("name", type=str, help="Name of video required", required=True)
-video_put_ags.add_argument("views", type=int, help="Views of video required", required=True)
-video_put_ags.add_argument("likes", type=int, help="Likes of video required", required=True)
+video_put_ags.add_argument("name", type=str, help="Name of video is required", required=True)
+video_put_ags.add_argument("views", type=int, help="Views of video is required", required=True)
+video_put_ags.add_argument("likes", type=int, help="Likes of video is required", required=True)
 
 video_update_args = reqparse.RequestParser()
 video_update_args.add_argument("name", type=str, help="Name of the video is required")
